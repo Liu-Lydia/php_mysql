ccbdcecaafbf5bff137f1f9a5b0b8e6de427d24b
Lydia Liu <tin20630@gmail.com>
2020-12-11 09:08:46 +0800

    1210練習更新

--- index_.php
+++ index_.php
@@ -1,6 +1,6 @@
-<?php include __DIR__ ."/parts/web_room.php"?>
-<?php include __DIR__ ."/parts/head.php"?>
-<?php include __DIR__ ."/parts/navbar.php"?>
+<?php include __DIR__ ."/parts/web_room.php";?>
+<?php include __DIR__ ."/parts/head.php";?>
+<?php include __DIR__ ."/parts/navbar.php";?>
 
 
 <?php include __DIR__ ."/parts/script.php"?>
