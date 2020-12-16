1d15d134bdbf2346861dbf644464adb51e245963
Lydia Liu <tin20630@gmail.com>
2020-12-16 16:25:56 +0800

    1216上課內容

--- ad_connect.php
+++ ad_connect.php
@@ -2,8 +2,8 @@
 
 require __DIR__ . '/db_connect.php';
 
-if(! isset($_SESSION['admin'])){
-  include __DIR__.'/ad_noconnect.php';
+if (!isset($_SESSION['admin'])) {
+  include __DIR__ . '/ad_noconnect.php';
   exit;
 }
 
@@ -13,15 +13,26 @@ $title = '通訊錄';
 $pageName = 'ad_connect';
 
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$search = isset($_GET['search']) ? ($_GET['search']) : '';
+$params = [];
+
+
+$where = 'WHERE 1';
+if (! empty($search)){
+  $where .= sprintf(" AND `name` LIKE %s" , $pdo -> quote('%' . $search . '%')) ;
+  $params['search'] = $search ;
+}
+
 
 $perPage = 3;
-$t_sql = "SELECT COUNT(1) FROM address_book";
+$t_sql = "SELECT COUNT(1) FROM address_book $where";
 $totalRows = $pdo->query($t_sql)->fetch()['COUNT(1)'];
 $totalPages = ceil($totalRows / $perPage);
-if ($page < 1) $page = 1;
-if ($page > $totalPages) $page = $totalPages;
 
-$p_sql = sprintf("SELECT * FROM address_book ORDER BY sid DESC LIMIT %s ,%s", ($page - 1) * $perPage, $perPage);
+if ($page > $totalPages) $page = $totalPages;
+if ($page < 1) $page = 1;
+
+$p_sql = sprintf("SELECT * FROM address_book %s ORDER BY sid DESC LIMIT %s ,%s",$where ,($page - 1) * $perPage, $perPage);
 
 $stmt = $pdo->query($p_sql);
 // $stmt = $pdo->query("SELECT * FROM address_book");
@@ -43,28 +54,38 @@ $stmt = $pdo->query($p_sql);
 
 <div class="container">
 
-  <div class="row">
+  <div class="row ">
     <div class="col">
       <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>"><a class="page-link" href="?page=1"><i class="fas fa-arrow-circle-left"></i></a></li>
-          <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>"><a class="page-link" href="?page=<?= $page - 1 ?>"><i class="far fa-arrow-alt-circle-left"></i></i></a></li>
+        <ul class="pagination m-0">
+          <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>"><a class="page-link" href="?<?php $params['page'] = 1 ; echo http_build_query($params); ?>"><i class="fas fa-arrow-circle-left"></i></a></li>
+          <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>"><a class="page-link" href="?<?php $params['page'] = $page-1 ; echo http_build_query($params); ?>"><i class="far fa-arrow-alt-circle-left"></i></i></a></li>
+
           <?php for ($i = $page - 5; $i <= $page + 5; $i++) :
             if ($i >= 1 and $i <= $totalPages) : ?>
-              <li class="page-item <?= $page == $i ? 'active' : '' ?>"><a class="page-link" href="?page=<?= $i ?>">
+
+              <li class="page-item <?= $page == $i ? 'active' : '' ?>"><a class="page-link" href="?<?php $params['page'] = $i ; echo http_build_query($params); ?>">
                   <?= $i ?>
                 </a></li>
           <?php endif;
           endfor; ?>
-          <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>"><a class="page-link" href="?page=<?= $page + 1 ?>"><i class="far fa-arrow-alt-circle-right"></i></i></a></li>
-          <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>"><a class="page-link" href="?page=<?= $totalPages ?>"><i class="fas fa-arrow-circle-right"></i></a></li>
+          <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>"><a class="page-link" href="?<?php $params['page'] = $page+1 ; echo http_build_query($params); ?>"><i class="far fa-arrow-alt-circle-right"></i></a></li>
+          <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>"><a class="page-link" href="?<?php $params['page'] = $totalPages ; echo http_build_query($params); ?>"><i class="fas fa-arrow-circle-right"></i></a></li>
         </ul>
       </nav>
     </div>
+
+    <div class="col d-flex flex-row-reverse bd-highlight">
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" name="search" value="<?= htmlentities($search) ?>" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+
   </div>
 
   <div class="row">
-    <div class="col">
+    <div class="col mt-4">
       <table class="table table-dark">
         <thead>
           <tr>
